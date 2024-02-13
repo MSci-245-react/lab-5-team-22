60bbd345847a5c84aa7952a2fddd20838f09d529
kirtibansal2002 <kirtibansalva@gmail.com>
2024-02-13 10:59:08 -0500

    finished lab 6 part 2

--- src/components/RestaurantList.spec.js
+++ src/components/RestaurantList.spec.js
@@ -3,30 +3,34 @@ import RestaurantList from './RestaurantList';
 
 
 describe('RestaurantList', () => {
-    it('loads restaurants on first render', () => {
-        const loadRestaurants = jest.fn().mockName('loadRestaurants');
-
-        const restaurants = [];
+    const restaurants = [
+        {id: 1, name: 'Sushi Place'},
+        {id: 2, name: 'Pizza Place'},
+    ];
+    
+    let loadRestaurants;
+        
+    function renderComponent() {
+        loadRestaurants = jest.fn().mockName('loadRestaurants');
+        
         render(
-         <RestaurantList
+            <RestaurantList
             loadRestaurants={loadRestaurants}
             restaurants={restaurants}
             />,
         );
+    }
+
+
+    it('loads restaurants on first render', () => {
+        renderComponent();
         
         expect(loadRestaurants).toHaveBeenCalled();
     });
 
     it('displays the restaurants', () => {
-        const noop = () => {};
-        const restaurants = [
-          {id: 1, name: 'Sushi Place'},
-          {id: 2, name: 'Pizza Place'},
-        ];
-      
-        render(<RestaurantList loadRestaurants={noop} restaurants={restaurants} />);
-
-            expect(screen.getByText('Sushi Place')).toBeInTheDocument();
-            expect(screen.getByText('Pizza Place')).toBeInTheDocument();
+        renderComponent();
+        expect(screen.getByText('Sushi Place')).toBeInTheDocument();
+        expect(screen.getByText('Pizza Place')).toBeInTheDocument();
       });
   });
